4143e7fe407eb8bf954f0ffd19c31125e50eef58
med salim ezzine <ezzinesalim21@gmail.com>
2026-06-20 08:06:58 +0100

    clean: remove old backend/ directory (replaced by elfatoora-middleware)

--- backend/src/routes/auth.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router, type Router as ExpressRouter } from "express";
-import { loginHandler, registerHandler } from "../controllers/auth.controller.js";
-
-export const authRouter: ExpressRouter = Router();
-
-authRouter.post("/register", registerHandler);
-authRouter.post("/login", loginHandler);
